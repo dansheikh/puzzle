a6526b84118d3198d72f17b149563738ecbb6be6
Dan Sheikh <dan.sheikh@outlook.com>
2017-07-13 00:45:59 -0400

    Added puzzle grid and basic word search.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,59 @@
 module Lib
-    ( someFunc
+    ( grid
+    , languages
+    , formatGrid
+    , outputGrid
+    , findWordInLine
+    , findWord
+    , findWords
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (isInfixOf)
+import Data.Maybe (catMaybes)
+
+type Grid = [String]
+
+formatGrid :: Grid -> String
+formatGrid = unlines
+
+outputGrid :: Grid -> IO ()
+outputGrid grid = putStrLn $ formatGrid grid
+
+findWordInLine :: String -> String -> Bool
+findWordInLine = isInfixOf
+
+findWord :: Grid -> String -> Maybe String
+findWord grid word =
+  let lines = grid ++ map reverse grid 
+      found = or $ map (findWordInLine word) lines
+  in if found then Just word else Nothing
+
+findWords grid words =
+  let foundWords = map (findWord grid) words
+  in catMaybes foundWords
+
+grid = ["__C________R___"
+       ,"__SI________U__"
+       ,"__HASKELL____B_"
+       ,"__A__A_____S__Y"
+       ,"__R___B___C____"
+       ,"__PHP____H_____"
+       ,"____S_LREP_____"
+       ,"____I__M_Y__L__"
+       ,"____I_E__T_O___"
+       ,"_________HB____"
+       ,"_________O_____"
+       ,"________CN_____"
+       ]
+
+languages = ["BASIC"
+            ,"COBOL"
+            ,"CSHARP"
+            ,"HASKELL"
+            ,"LISP"
+            ,"PERL"
+            ,"PHP"
+            ,"PYTHON"
+            ,"RUBY"
+            ,"SCHEME"
+            ]
