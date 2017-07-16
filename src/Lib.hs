7e0772798a65b617ada0233d7f4212bf99d72dea
Dan Sheikh <dan.sheikh@outlook.com>
2017-07-16 01:26:16 -0400

    Made word search recursive.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,36 +1,66 @@
 module Lib
-    ( formatGrid
+    ( Cell(Cell, Indent)
+    , cellToChar
+    , formatGrid
     , outputGrid
+    , zipOverGrid
+    , zipOverGridWith
+    , coordsGrid
+    , gridWithCoords
     , findWordInLine
+    , findWordInCellLinePrefix
     , skew
     , getLines
     , findWord
     , findWords
     ) where
 
-import Data.List (isInfixOf, transpose)
-import Data.Maybe (catMaybes)
+import Data.List (isInfixOf, transpose, zipWith)
+import Data.Maybe (catMaybes, listToMaybe)
 
-type Grid = [String]
+type Grid a = [[a]]
 
-formatGrid :: Grid -> String
-formatGrid = unlines
+data Cell = Cell (Integer, Integer) Char
+          | Indent
+          deriving (Eq, Ord, Show)
 
-outputGrid :: Grid -> IO ()
+cellToChar :: Cell -> Char
+cellToChar (Cell _ c) = c
+cellToChar Indent = '?'
+
+mapOverGrid :: (a -> b) -> Grid a -> Grid b
+mapOverGrid = map . map
+
+formatGrid :: Grid Cell -> String
+formatGrid = unlines . mapOverGrid cellToChar
+
+outputGrid :: Grid Cell -> IO ()
 outputGrid grid = putStrLn $ formatGrid grid
 
-findWordInLine :: String -> String -> Bool
-findWordInLine = isInfixOf
+zipOverGrid :: Grid a -> Grid b -> Grid (a, b)
+zipOverGrid = zipWith zip
 
-skew :: Grid -> Grid
+zipOverGridWith :: (a -> b -> c) -> Grid a -> Grid b -> Grid c
+zipOverGridWith = zipWith . zipWith
+
+coordsGrid :: Grid (Integer, Integer)
+coordsGrid =
+  let rows = map repeat [0..]
+      cols = repeat [0..]
+  in zipOverGrid rows cols
+
+gridWithCoords :: Grid Char -> Grid Cell
+gridWithCoords = zipOverGridWith Cell coordsGrid
+
+skew :: Grid Cell -> Grid Cell
 skew [] = []
 skew (x:xs) = x : skew (map indent xs)
-  where indent line = '_' : line
+  where indent line = Indent : line
 
-diagonalize :: Grid -> Grid
+diagonalize :: Grid Cell -> Grid Cell
 diagonalize = transpose . skew
 
-getLines :: Grid -> [String]
+getLines :: Grid Cell -> Grid Cell
 getLines grid = 
   let horizontal = grid
       vertical = transpose grid
@@ -39,12 +69,27 @@ getLines grid =
       lines = horizontal ++ vertical ++ diagonal ++ altDiagonal
   in lines ++ map reverse lines
 
-findWord :: Grid -> String -> Maybe String
+findWordInLine :: String -> [Cell] -> Maybe [Cell]
+findWordInLine _ [] = Nothing
+findWordInLine word line =
+  let found = findWordInCellLinePrefix [] word line
+  in case found of
+    Nothing -> findWordInLine word (tail line)
+    cs@(Just _) -> cs
+
+findWordInCellLinePrefix :: [Cell] -> String -> [Cell] -> Maybe [Cell]
+findWordInCellLinePrefix acc (x:xs) (c:cs)
+  | x == cellToChar c = findWordInCellLinePrefix (c : acc) xs cs
+findWordInCellLinePrefix acc [] _ = Just $ reverse acc
+findWordInCellLinePrefix _ _ _ = Nothing
+
+findWord :: Grid Cell -> String -> Maybe [Cell]
 findWord grid word =
   let lines = getLines grid
-      found = or $ map (findWordInLine word) lines
-  in if found then Just word else Nothing
+      foundWords = map (findWordInLine word) lines
+  in listToMaybe (catMaybes foundWords)
 
+findWords :: Grid Cell -> [String] -> [[Cell]]
 findWords grid words =
   let foundWords = map (findWord grid) words
   in catMaybes foundWords
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,21 +2,30 @@ import Test.Hspec
 import Lib
 import Data
 
+gwc = gridWithCoords grid
+
+testFindWord word =
+  let (Just result) = findWord gwc word
+      string = map cellToChar result
+  in string `shouldBe` word
+
 main :: IO ()
 main = hspec $ do
   describe "formatGrid" $ do
     it "should concatenate every line with a newline" $ do
-      (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
+      (formatGrid (gridWithCoords ["abc", "def", "ghi"])) `shouldBe` "abc\ndef\nghi\n"
 
   describe "findWord" $ do
     it "should find the supplied word if it is present in the grid" $ do
-      findWord grid "HASKELL" `shouldBe` Just "HASKELL"
-      findWord grid "RUBY" `shouldBe` Just "RUBY"
+      testFindWord "HASKELL"
+      testFindWord "PERL"
     it "should not find the supplied word if it is not present in the grid" $ do
-      findWord grid "PURESCRIPT" `shouldBe` Nothing
+      findWord gwc "PURESCRIPT" `shouldBe` Nothing
 
   describe "findWords" $ do
     it "should find all the words in the supplied list that are present the grid" $ do
-      findWords grid languages `shouldBe` languages
+      let found = findWords gwc languages
+          foundString = map (map cellToChar) found
+      foundString `shouldBe` languages
     it "should not find any words in the supplied list that are not present the grid" $ do
-      findWords grid ["FRENCH", "ENGLISH", "GERMAN"] `shouldBe` []
+      findWords gwc ["FRENCH", "ENGLISH", "GERMAN"] `shouldBe` []
